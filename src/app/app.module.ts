35a1682370b5865df69d327b2ccdd051654e0e87
diegroxdibp <diegrox.rox@gmail.com>
2022-01-22 15:38:13 +0000

    movie ranking styled

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
-import { MatIconModule } from '@angular/material/icon';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -12,12 +12,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MovieRankingComponent } from './movie-ranking/movie-ranking.component';
 import { NavbarTopComponent } from './navbar-top/navbar-top.component';
+import { PillComponent } from './pill/pill.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MovieRankingComponent,
-    NavbarTopComponent
+    NavbarTopComponent,
+    PillComponent
   ],
   imports: [
     BrowserModule,
